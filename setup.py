b285435e33130ed850481b350c66761427876154
peppelinux <giuseppe.demarco@unical.it>
2021-06-12 14:51:49 +0200

    fix: setup.py and fews packaging things

--- setup.py
+++ setup.py
@@ -1,22 +1,24 @@
+import os
+
 from setuptools import setup
 
-def readme():
-    with open('README.md') as f:
-        return f.read()
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
 
 setup(name='django-auto-serializer',
-      version='0.4.2',
-      description="Django app that automates objects tree serialization recursively",
-      long_description=readme(),
-      classifiers=['Development Status :: 5 - Production/Stable',
-                  'License :: OSI Approved :: BSD License',
-                  'Programming Language :: Python :: 3 :: Only'],
-      url='https://github.com/peppelinux/django-auto-serializer',
-      author='Giuseppe De Marco',
-      author_email='giuseppe.demarco@unical.it',
-      license='BSD',
-      packages=['django_auto_serializer'],
-      install_requires=[
-        'django>=2.0,<4.0',
-      ],
-     )
+    version='0.4.2',
+    description="Django app that automates objects tree serialization recursively",
+    long_description=README,
+    long_description_content_type='text/markdown',
+    classifiers=['Development Status :: 5 - Production/Stable',
+              'License :: OSI Approved :: BSD License',
+              'Programming Language :: Python :: 3 :: Only'],
+    url='https://github.com/peppelinux/django-auto-serializer',
+    author='Giuseppe De Marco',
+    author_email='giuseppe.demarco@unical.it',
+    license='BSD',
+    packages=['django_auto_serializer'],
+    install_requires=[
+    'django>=2.0,<4.0',
+    ],
+)
